70001afcfd9bd7566a6dd6b3014fb5e3ff99820a
Shadin Cornelio <shadincornelio@Shadins-MacBook-Pro.local>
2017-05-07 19:54:08 -0700

    Running version

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -8,18 +8,67 @@
 
 import UIKit
 
+
+func shortNameFromName(name:String) -> String {
+    var shortName = name
+    shortName.remove(at: shortName.startIndex)
+    return shortName.lowercased()
+}
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    
+    let shortName = shortNameFromName(name: fullName)
+    
+    let lyrics = lyricsTemplate
+        .replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+        .replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+    
+    return lyrics
+}
+
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joined(separator: "\n")
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
 class ViewController: UIViewController {
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
+    @IBAction func reset(_ sender: Any) {
+        nameField.text=""
+        lyricsView.text=""
+    }
+    
+    @IBAction func displayLyrics(_ sender: Any) {
+        lyricsView.text=lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: nameField.text!)
+    }
 
 }
+
+
+
+
 
